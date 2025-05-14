a2d695e8741b9669cf035fa9978f559580be49ac
DELL <IT24102390@my.sliit.lk>
2025-05-15 02:39:59 +0530

    FixTypoInVariableName

--- enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
@@ -14,7 +14,7 @@ public class OfferDAO {
     public OfferDAO() {
         ensureFileExists();
     }
-//ImprovedMethodNamingForClarity
+//FixTypoInVariableName
     // Read all Offers from the text file
     public List<Offer> getAllOffers() {
         List<Offer> offers = new ArrayList<>();
