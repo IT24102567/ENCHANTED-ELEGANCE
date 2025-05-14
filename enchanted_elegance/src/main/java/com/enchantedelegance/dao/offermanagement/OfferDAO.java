2214887370d7098fedba39ad2433a0f3ab982738
DELL <IT24102390@my.sliit.lk>
2025-05-14 22:55:26 +0530

    UpdatedDocumentation

--- enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
@@ -14,7 +14,7 @@ public class OfferDAO {
     public OfferDAO() {
         ensureFileExists();
     }
-//ImprovedUI
+//UpdateDocumentation
     // Read all Offers from the text file
     public List<Offer> getAllOffers() {
         List<Offer> offers = new ArrayList<>();
