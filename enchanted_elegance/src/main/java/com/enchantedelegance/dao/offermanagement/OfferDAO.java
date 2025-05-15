04b3cef67bfc7eb67a24a9c9591dacbe51895e34
DELL <IT24102390@my.sliit.lk>
2025-05-15 21:18:21 +0530

    Update OfferDAOToSupportNewDiscountFormat

--- enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
@@ -14,7 +14,7 @@ public class OfferDAO {
     public OfferDAO() {
         ensureFileExists();
     }
-//Verify deleteOfferByIdWorksAsExpected
+//Update OfferDAOToSupportNewDiscountFormat
     // Read all Offers from the text file
     public List<Offer> getAllOffers() {
         List<Offer> offers = new ArrayList<>();
