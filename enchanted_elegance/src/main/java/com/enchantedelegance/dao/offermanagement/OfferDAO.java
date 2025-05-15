df0b37f2fbff0dab63021ce4adf6e009f5083121
DELL <IT24102390@my.sliit.lk>
2025-05-15 23:32:05 +0530

    AvoidDuplicateOfferIDs

--- enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
@@ -14,7 +14,7 @@ public class OfferDAO {
     public OfferDAO() {
         ensureFileExists();
     }
-//FixIssueWithReadingEmptyLinesInFile
+//AvoidDuplicateOfferIDs
     // Read all Offers from the text file
     public List<Offer> getAllOffers() {
         List<Offer> offers = new ArrayList<>();
