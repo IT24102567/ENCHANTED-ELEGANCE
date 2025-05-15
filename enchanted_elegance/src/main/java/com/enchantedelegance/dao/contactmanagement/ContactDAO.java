3ee2a0fa4dbbdcba605f325a1e1158f955b596a9
it24102764 <it24102764@my.sliit.lk>
2025-05-15 23:36:34 +0530

    getAllProcessingContact added

--- enchanted_elegance/src/main/java/com/enchantedelegance/dao/contactmanagement/ContactDAO.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/dao/contactmanagement/ContactDAO.java
@@ -73,8 +73,8 @@ public class ContactDAO {
     // Get all processing contacts
     public List<Contact> getAllProcessingContact() {
         List<Contact> processingContact = new ArrayList<>();
-        List<Contact> contacts = this.getAllContacts();
         //
+        List<Contact> contacts = this.getAllContacts();
 
         for (Contact contact : contacts) {
             if (contact.getStatus().equalsIgnoreCase("processing")) {
