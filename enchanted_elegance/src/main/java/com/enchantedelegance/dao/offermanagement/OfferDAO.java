dc24ede246270134a74b4ea9ea72b3d78eab36b9
DELL <IT24102390@my.sliit.lk>
2025-05-15 20:51:39 +0530

    CleanUpUnusedVariables

--- enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
@@ -14,7 +14,7 @@ public class OfferDAO {
     public OfferDAO() {
         ensureFileExists();
     }
-//ImproveComments
+//CleanUpUnusedVariables
     // Read all Offers from the text file
     public List<Offer> getAllOffers() {
         List<Offer> offers = new ArrayList<>();
