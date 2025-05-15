5695f7088b31d18662a806638008b3f85ace6db5
it24102764 <it24102764@my.sliit.lk>
2025-05-15 23:26:36 +0530

    fix null contact error

--- enchanted_elegance/src/main/java/com/enchantedelegance/dao/contactmanagement/ContactDAO.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/dao/contactmanagement/ContactDAO.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class ContactDAO {
     private static final String FILE_PATH =  RootPath.rootPath+"/src/main/resources/data/contactmanagement/contact.txt"; // Updated file path
     private static final String DELIMITER = ","; // Delimiter for separating fields
+    //
 
     public ContactDAO() {
         ensureFileExists();
@@ -37,7 +38,6 @@ public class ContactDAO {
                     contacts.add(contact);
                 }
             }
-            //
         } catch (IOException e) {
             e.printStackTrace();
         }
