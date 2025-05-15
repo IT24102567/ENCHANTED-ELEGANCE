e0ab7e1af65b55bf15d8ccfc1c50ce5257c43fc3
DELL <IT24102390@my.sliit.lk>
2025-05-15 20:15:23 +0530

    UpdateExistingOfferData

--- enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/dao/offermanagement/OfferDAO.java
@@ -14,7 +14,7 @@ public class OfferDAO {
     public OfferDAO() {
         ensureFileExists();
     }
-//UpdateOfferStructureWithImageURL
+//UpdateExistingOfferData
     // Read all Offers from the text file
     public List<Offer> getAllOffers() {
         List<Offer> offers = new ArrayList<>();
