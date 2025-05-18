869a92482516e5bd76ede960afcf20672c32c5ca
Nethmi <it24104013@my.sliit.lk>
2025-05-18 09:24:39 +0530

    fix error

--- enchanted_elegance/src/main/java/com/enchantedelegance/dao/bookingmanagement/BookingDAO.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/dao/bookingmanagement/BookingDAO.java
@@ -90,7 +90,7 @@ public class BookingDAO {
     // Update an existing booking's details
     public boolean updateBooking(Booking updatedBooking) {
         List<Booking> bookings = getAllBookings();
-        boolean updated = false;//
+        boolean updated = false;
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
             for (Booking booking : bookings) {
@@ -116,7 +116,7 @@ public class BookingDAO {
     // Delete booking by ID
     public boolean deleteBookingById(int id) {
         List<Booking> bookings = getAllBookings();
-        boolean deleted = false;
+        boolean deleted = false;//
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
             for (Booking booking : bookings) {
